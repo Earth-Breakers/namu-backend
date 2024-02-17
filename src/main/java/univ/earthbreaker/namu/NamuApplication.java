4e9bccebaaa21e92bc26d52361d9ef24a271c731
chanwoo <chocw0402@gmail.com>
2024-02-17 18:55:09 +0900

    initial commit

---
+++ src/main/java/univ/earthbreaker/namu/NamuApplication.java
@@ -0,0 +1,13 @@
+package univ.earthbreaker.namu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/univ/earthbreaker/namu/NamuApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.earthbreaker.namu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
