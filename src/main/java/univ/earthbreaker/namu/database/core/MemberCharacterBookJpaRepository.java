a070694d2463de6eeeda4dcf2ebac7bc6535df29
chanwoo <chocw0402@gmail.com>
2024-02-22 18:47:12 +0900

    feature : member character book jpa 엔티티, 레포지토리 개발

---
+++ src/main/java/univ/earthbreaker/namu/database/core/MemberCharacterBookJpaRepository.java
@@ -0,0 +1,6 @@
+package univ.earthbreaker.namu.database.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberCharacterBookJpaRepository extends JpaRepository<MemberCharacterBookJpaEntity, Long> {
+}
