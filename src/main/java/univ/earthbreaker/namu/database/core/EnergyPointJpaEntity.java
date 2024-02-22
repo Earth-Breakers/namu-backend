5881580423c2cc0202b37c3c3bdc2c54875ff7f7
조찬우 <58971262+oownahcohc@users.noreply.github.com>
2024-02-22 18:51:50 +0900

    feature : database 초기 엔티티 구성 및 개발 (#7)
    * feature : account jpa 엔티티, 레포지토리 개발
    
    * feature : character jpa 엔티티, 레포지토리 개발
    
    * feature : current character jpa 엔티티, 레포지토리 개발
    
    * feature : energy point jpa 엔티티, 레포지토리 개발
    
    * feature : member jpa 엔티티, 레포지토리 개발
    
    * feature : member character book jpa 엔티티, 레포지토리 개발
    
    * add : 내장 H2 데이터베이스 이름 변경
    
    * feature : domain enum 클래스 개발
    
    * config : database yml 설정
    
    * config : application yml 에 databse yml 설정 추가

---
+++ src/main/java/univ/earthbreaker/namu/database/core/EnergyPointJpaEntity.java
@@ -0,0 +1,26 @@
+package univ.earthbreaker.namu.database.core;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "energy_point")
+public class EnergyPointJpaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@Column(nullable = false)
+	private Integer point;
+
+	@Column(nullable = false)
+	private Long memberNo;
+
+	protected EnergyPointJpaEntity() {
+	}
+}
