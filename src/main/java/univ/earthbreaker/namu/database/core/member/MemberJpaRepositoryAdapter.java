196014e4928337e2ce825fb2b8a9040ed90a54af
chanwoo <chocw0402@gmail.com>
2024-02-26 13:20:55 +0900

    feature : core member database 개발

---
+++ src/main/java/univ/earthbreaker/namu/database/core/member/MemberJpaRepositoryAdapter.java
@@ -0,0 +1,28 @@
+package univ.earthbreaker.namu.database.core.member;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.stereotype.Repository;
+
+import univ.earthbreaker.namu.core.domain.member.MemberRepository;
+
+@Repository
+public class MemberJpaRepositoryAdapter implements MemberRepository {
+
+	private final MemberJpaRepository memberJpaRepository;
+
+	public MemberJpaRepositoryAdapter(MemberJpaRepository memberJpaRepository) {
+		this.memberJpaRepository = memberJpaRepository;
+	}
+
+	@Override
+	public @Nullable Long findMemberNoOrNull(String socialId) {
+		return null;
+	}
+
+	@Override
+	public @NotNull Long create(String socialNickname) {
+		MemberJpaEntity memberJpaEntity = memberJpaRepository.save(MemberJpaEntity.create(socialNickname));
+		return memberJpaEntity.getNo();
+	}
+}
