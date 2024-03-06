381bc882137224316b2c2d77abe958554519995c
chanwoo <chocw0402@gmail.com>
2024-03-06 22:41:54 +0900

    refactor : commonExceptionDocumentResultHandler 공통 메서드 사용

--- src/test/java/univ/earthbreaker/namu/core/api/CommonExceptionControllerTest.java
+++ src/test/java/univ/earthbreaker/namu/core/api/CommonExceptionControllerTest.java
@@ -1,14 +1,7 @@
 package univ.earthbreaker.namu.core.api;
 
-import static univ.earthbreaker.namu.support.apidocs.ApiDocsUtils.API_DOCUMENT_IDENTIFIER;
-import static univ.earthbreaker.namu.support.apidocs.ApiDocsUtils.operationRequestPreprocessor;
-import static univ.earthbreaker.namu.support.apidocs.ApiDocsUtils.operationResponsePreprocessor;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 class CommonExceptionControllerTest extends PresentationTest {
@@ -22,15 +15,6 @@ class CommonExceptionControllerTest extends PresentationTest {
 		// apidocs
 		whenPost("/test/exception", new CommonExceptionController.ExceptionRequest("body"))
 			.andExpect(MockMvcResultMatchers.status().isInternalServerError())
-			.andDo(
-				MockMvcRestDocumentation.document(
-					API_DOCUMENT_IDENTIFIER,
-					operationRequestPreprocessor(),
-					operationResponsePreprocessor(),
-					PayloadDocumentation.responseFields(
-						PayloadDocumentation.fieldWithPath("message").type(JsonFieldType.STRING).description("예외 메시지")
-					)
-				)
-			);
+			.andDo(commonExceptionDocumentResultHandler());
 	}
 }
