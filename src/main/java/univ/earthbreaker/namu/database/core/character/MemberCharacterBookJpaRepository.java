af538decddef6c8d89449407c447c750f5a3293d
chanwoo <chocw0402@gmail.com>
2024-03-11 14:48:37 +0900

    feature : 캐릭터 도감 조회 database 개발

--- src/main/java/univ/earthbreaker/namu/database/core/character/MemberCharacterBookJpaRepository.java
+++ src/main/java/univ/earthbreaker/namu/database/core/character/MemberCharacterBookJpaRepository.java
@@ -1,6 +1,21 @@
-package univ.earthbreaker.namu.database.core;
+package univ.earthbreaker.namu.database.core.character;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberCharacterBookJpaRepository extends JpaRepository<MemberCharacterBookJpaEntity, Long> {
+
+	@Query("""
+		SELECT
+		    mcb.no, mcb.memberNo, mcb.characterNo, mcb.count,
+		    c.type, c.thumbnailImagePath,
+		    COUNT(c) OVER (PARTITION BY c.type) AS totalCountPerType,
+		    CASE WHEN mcb.no IS NOT NULL THEN TRUE ELSE FALSE END AS isAcquired
+		FROM MemberCharacterBookJpaEntity mcb
+		    RIGHT JOIN CharacterJpaEntity c
+		        ON mcb.characterNo = c.no AND mcb.memberNo = :memberNo
+		ORDER BY mcb.characterNo""")
+	List<MemberCharacterBookProjection> findMemberCharacterBookByMemberNo(long memberNo);
 }
