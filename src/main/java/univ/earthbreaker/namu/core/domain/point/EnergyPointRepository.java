01d1b67ad9bce5372b982ec5ca35fbe8e8a2cd12
chanwoo <chocw0402@gmail.com>
2024-03-27 23:15:37 +0900

    refactor : point -> energy point 로 이름 변경

--- src/main/java/univ/earthbreaker/namu/core/domain/point/EnergyPointRepository.java
+++ src/main/java/univ/earthbreaker/namu/core/domain/point/EnergyPointRepository.java
@@ -2,7 +2,12 @@ package univ.earthbreaker.namu.core.domain.point;
 
 import org.springframework.stereotype.Repository;
 
+import jakarta.validation.constraints.NotNull;
+
 @Repository
 public interface PointRepository {
+
+	@NotNull Point find(long memberNo);
+
 	void update(PointUpdateDbCommand command);
 }
