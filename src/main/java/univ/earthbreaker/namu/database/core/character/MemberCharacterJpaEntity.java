cfa4f995a7c375dcef8dc9d948516e56f9be3755
chanwoo <chocw0402@gmail.com>
2024-03-28 23:46:45 +0900

    feature : 회원의 캐릭터 도감 조회하기 database 개발

--- src/main/java/univ/earthbreaker/namu/database/core/character/MemberCharacterJpaEntity.java
+++ src/main/java/univ/earthbreaker/namu/database/core/character/MemberCharacterJpaEntity.java
@@ -1,15 +1,18 @@
 package univ.earthbreaker.namu.database.core.character;
 
+import org.jetbrains.annotations.NotNull;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import univ.earthbreaker.namu.core.domain.character.book.MemberCharacter;
 
 @Entity
-@Table(name = "member_character_book")
-public class MemberCharacterBookJpaEntity {
+@Table(name = "member_character")
+public class MemberCharacterJpaEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +27,15 @@ public class MemberCharacterBookJpaEntity {
 	@Column(nullable = false)
 	private Integer count;
 
-	protected MemberCharacterBookJpaEntity() {
+	protected MemberCharacterJpaEntity() {
+	}
+
+	MemberCharacter toMemberCharacter(@NotNull CharacterJpaEntity characterJpaEntity) {
+		return new MemberCharacter(
+			no,
+			memberNo,
+			count,
+			characterJpaEntity.toNamuCharacter()
+		);
 	}
 }
--- src/main/java/univ/earthbreaker/namu/database/core/character/MemberCharacterJpaRepository.java
+++ src/main/java/univ/earthbreaker/namu/database/core/character/MemberCharacterJpaRepository.java
@@ -2,22 +2,23 @@ package univ.earthbreaker.namu.database.core.character;
 
 import java.util.List;
 
+import org.jetbrains.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface MemberCharacterBookJpaRepository extends JpaRepository<MemberCharacterBookJpaEntity, Long> {
+public interface MemberCharacterJpaRepository extends JpaRepository<MemberCharacterJpaEntity, Long> {
 
-	
+	@Nullable MemberCharacterJpaEntity findByMemberNoAndCharacterNo(long memberNo, long characterNo);
 
 	@Query("""
 		SELECT
-		    mcb.no, mcb.memberNo, mcb.characterNo, mcb.count,
+		    mc.no, mc.memberNo, mc.characterNo, mc.count,
 		    c.type, c.thumbnailImagePath,
 		    COUNT(c) OVER (PARTITION BY c.type) AS totalCountPerType,
-		    CASE WHEN mcb.no IS NOT NULL THEN TRUE ELSE FALSE END AS isAcquired
-		FROM MemberCharacterBookJpaEntity mcb
+		    CASE WHEN mc.no IS NOT NULL THEN TRUE ELSE FALSE END AS isAcquired
+		FROM MemberCharacterJpaEntity mc
 		    RIGHT JOIN CharacterJpaEntity c
-		        ON mcb.characterNo = c.no AND mcb.memberNo = :memberNo
-		ORDER BY mcb.characterNo""")
+		        ON mc.characterNo = c.no AND mc.memberNo = :memberNo
+		ORDER BY mc.characterNo""")
 	List<MemberCharacterBookProjection> findMemberCharacterBookByMemberNo(long memberNo);
 }
