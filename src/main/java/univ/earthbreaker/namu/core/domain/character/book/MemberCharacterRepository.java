839e710573d0d870231262b2693f11f02f096c95
chanwoo <chocw0402@gmail.com>
2024-03-29 18:41:36 +0900

    feature : 캐릭터 최종 진화 시 캐릭터 도감 이동 이벤트 domain 개발

--- src/main/java/univ/earthbreaker/namu/core/domain/character/book/MemberCharacterRepository.java
+++ src/main/java/univ/earthbreaker/namu/core/domain/character/book/MemberCharacterRepository.java
@@ -1,5 +1,7 @@
 package univ.earthbreaker.namu.core.domain.character.book;
 
+import static univ.earthbreaker.namu.core.domain.character.book.CharacterBookEventHandler.AddFinalCharacterDbCommand;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,6 @@ public interface MemberCharacterRepository {
 	@Nullable MemberCharacter findOrNull(long memberNo, long characterNo);
 
 	@NotNull CharacterBook findBook(long memberNo);
+
+	void createOrUpdate(AddFinalCharacterDbCommand command);
 }
