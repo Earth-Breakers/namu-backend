67526eeae89a20b3885776376144bbda5b347faf
조찬우 <58971262+oownahcohc@users.noreply.github.com>
2024-03-31 00:51:49 +0900

    test : .http api 호출 테스트 (#59)
    * fix : kakao client response 객체 접근 제한자 수정
    
    * gitginore : auth .http 파일

--- src/main/java/univ/earthbreaker/namu/external/oauth/KakaoMemberInfoResponse.java
+++ src/main/java/univ/earthbreaker/namu/external/oauth/KakaoMemberInfoResponse.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-record KakaoMemberInfoResponse(String id, KakaoAccount kakaoAccount) {
+public record KakaoMemberInfoResponse(String id, KakaoAccount kakaoAccount) {
 
 	@NotNull OAuthMemberInfoResult toResult() {
 		return new OAuthMemberInfoResult(id, kakaoAccount().profileNickname());
