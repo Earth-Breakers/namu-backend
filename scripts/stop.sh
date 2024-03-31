35914470545818827d8cc351cbdaea865ffe9767
조찬우 <58971262+oownahcohc@users.noreply.github.com>
2024-04-01 03:17:03 +0900

    infra : ci/cd 관련 작업 (#63)
    * infra : ci yml 설정
    
    * infra : cd yml 설정
    
    * infra : 배포 스크립트 및 배포 종료 스크립트
    
    * infra : code deploy 배포 프로세스 이후에 수행할 스크립트
    
    * gitginore : firebase gitignore 제거
    
    * config : submodule 을 통한 yml 관리
    
    * infra : aws s3 설정
    
    * infra : ci.yml 수정
    
    * infra : build with gradle uses 수정
    
    * infra : build with gradle uses 수정
    
    * infra : cd.yml submodule config 수정
    
    * infra : health check 도입

---
+++ scripts/stop.sh
@@ -0,0 +1,12 @@
+BASE_PATH="/home/ubuntu"
+DEPLOY_LOG="$BASE_PATH/deploy.log"
+CURRENT_TIME=$(date +%c)
+
+CURRENT_PID=$(lsof -t -i tcp:8080)
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "$CURRENT_TIME > $CURRENT_PID pid 에서 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  kill -15 "$CURRENT_PID"
+  echo "$CURRENT_TIME > $CURRENT_PID pid 에서 실행중인 애플리케이션을 종료합니다" >> $DEPLOY_LOG
+fi
