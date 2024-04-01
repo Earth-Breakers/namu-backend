bad766885de254c29bbf2e568908d5553e15304d
chanwoo <chocw0402@gmail.com>
2024-04-01 15:01:25 +0900

    chore : 주석 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,13 +16,6 @@ SPRING_PROFILES_ACTIVE="dev"
 IMAGE_ACCESS_URL="https://namu-bucket.s3.ap-northeast-2.amazonaws.com/"
 LOG4J_CONTEXT_SELECTOR="org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 
-#COMMAND="java -jar -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE} \
-#-DimageAccessUrl=${IMAGE_ACCESS_URL} \
-#-DLog4jContextSelector=${LOG4J_CONTEXT_SELECTOR} \
-#-Dlog4j2.enable.threadlocals=${LOG4J2_ENABLE_THREADLOCALS} \
-#-Dlog4j2.enable.direct.encoders=${LOG4J2_ENABLE_DIRECT_ENCODERS} \
-#${BUILD_JAR_FILE}"
-
 nohup java -jar -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -DimageAccessUrl=$IMAGE_ACCESS_URL -DLog4jContextSelector=$LOG4J_CONTEXT_SELECTOR -Dlog4j2.enable.threadlocals=true -Dlog4j2.enable.direct.encoders=true "$BUILD_JAR_FILE" > $APP_LOG 2>&1 &
 echo "$CURRENT_TIME > build jar 파일 실헹" >> $DEPLOY_LOG
 
