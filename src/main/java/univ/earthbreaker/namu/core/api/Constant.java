27f7d824eea7532f70475a60a6d5df646a152dee
chanwoo <chocw0402@gmail.com>
2024-04-07 16:29:40 +0900

    refactor : Constant 모듈 변경

--- src/main/java/univ/earthbreaker/namu/core/api/Constant.java
+++
@@ -1,6 +0,0 @@
-package univ.earthbreaker.namu.core.api;
-
-public final class Constant {
-
-	public static final String IMAGE_ACCESS_URL = "image.access.url";
-}
---
+++ src/main/java/univ/earthbreaker/namu/core/domain/common/Constant.java
@@ -0,0 +1,7 @@
+package univ.earthbreaker.namu.core.api;
+
+public final class Constant {
+
+	public static final String IMAGE_SYSTEM_PROPERTY = "image.access.url";
+	public static final String IMAGE_ACCESS_URL = System.getProperty(IMAGE_SYSTEM_PROPERTY);
+}
