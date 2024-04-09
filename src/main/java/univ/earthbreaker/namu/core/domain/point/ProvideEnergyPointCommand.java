ad6d285d3c94cdaf87537265ea0e37d95516adc8
조찬우 <58971262+oownahcohc@users.noreply.github.com>
2024-04-09 17:46:41 +0900

    fix :  (#87)
    * fix : 캐릭터에게 제공하는 energy type 에 대한 validate 추가
    
    * fix : native query 수행 시 테이블 명 불일치 및 enum 파라미터의 바인딩 타입 불일치로 인한 오류 수정
    
    * fix : validate 위치 수정

--- src/main/java/univ/earthbreaker/namu/core/domain/point/ProvideEnergyPointCommand.java
+++ src/main/java/univ/earthbreaker/namu/core/domain/point/ProvideEnergyPointCommand.java
@@ -6,15 +6,24 @@ import univ.earthbreaker.namu.core.domain.common.SelfValidating;
 
 public class ProvideEnergyPointCommand extends SelfValidating<ProvideEnergyPointCommand> {
 
+	private static final String NOT_ALLOWED_ENERGY_TYPE = "DEFAULT";
+
 	private final @NotNull Long memberNo;
 	private final @NotNull Integer point;
-	private final @NotBlank String energyType;
+	private final @NotNull @NotBlank String energyType;
 
 	public ProvideEnergyPointCommand(Long memberNo, Integer point, String energyType) {
 		this.memberNo = memberNo;
 		this.point = point;
 		this.energyType = energyType;
-		this.validateSelf("memberNo, point 는 null 이 될 수 없고, energyType 는 공백일 수 없습니다");
+		this.validateEnergyType(energyType);
+		this.validateSelf("memberNo, point 는 null 이 될 수 없고, energyType 는 null 혹은 공백일 수 없습니다");
+	}
+
+	private void validateEnergyType(String energyType) {
+		if (energyType.equals(NOT_ALLOWED_ENERGY_TYPE)) {
+			throw new IllegalArgumentException("캐릭터에게 제공할 에너지 타입은 DEFAULT 일 수 없습니다");
+		}
 	}
 
 	public Long getMemberNo() {
