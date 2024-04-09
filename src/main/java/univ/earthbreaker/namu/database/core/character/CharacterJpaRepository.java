3f5dccf0d621a4487b73399b71578fa885f69387
조찬우 <58971262+oownahcohc@users.noreply.github.com>
2024-04-09 17:46:41 +0900

    fix : 캐릭터 최종 성장 시 오류 수정 (#87)
    * fix : 캐릭터에게 제공하는 energy type 에 대한 validate 추가
    
    * fix : native query 수행 시 테이블 명 불일치 및 enum 파라미터의 바인딩 타입 불일치로 인한 오류 수정
    
    * fix : validate 위치 수정

--- src/main/java/univ/earthbreaker/namu/database/core/character/CharacterJpaRepository.java
+++ src/main/java/univ/earthbreaker/namu/database/core/character/CharacterJpaRepository.java
@@ -17,12 +17,12 @@ public interface CharacterJpaRepository extends JpaRepository<CharacterJpaEntity
 
 	@Query(
 		value = """
-			SELECT c.* FROM character c
-			WHERE c.level = :level
-			    AND c.group_number = :groupNumber
-			    AND c.is_endangered = :isEndangered
-			    AND c.type = :type
+			SELECT nc.* FROM namu_character nc
+			WHERE nc.level = :level
+			    AND nc.group_number = :groupNumber
+			    AND nc.is_endangered = :isEndangered
+			    AND nc.type = :type
 			ORDER BY RAND() LIMIT 1""",
 		nativeQuery = true)
-	@Nullable CharacterJpaEntity findRandomBy(int level, int groupNumber, boolean isEndangered, CharacterType type);
+	@Nullable CharacterJpaEntity findRandomBy(int level, int groupNumber, boolean isEndangered, String type);
 }
