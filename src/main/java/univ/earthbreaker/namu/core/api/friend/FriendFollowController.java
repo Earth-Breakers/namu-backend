a76907df88bf35680245254b175d66ae686af762
chanwoo <chocw0402@gmail.com>
2024-04-16 15:13:10 +0900

    feature : 친구 팔로우 시 알림 전송 domain 구현

--- src/main/java/univ/earthbreaker/namu/core/api/friend/FriendFollowController.java
+++ src/main/java/univ/earthbreaker/namu/core/api/friend/FriendFollowController.java
@@ -8,23 +8,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import univ.earthbreaker.namu.core.api.auth.support.AuthMapping;
 import univ.earthbreaker.namu.core.api.auth.support.LoginMember;
+import univ.earthbreaker.namu.core.domain.member.friend.FollowFriendPushNotificationBridge.FollowResult;
 import univ.earthbreaker.namu.core.domain.member.friend.FriendFollowService;
 import univ.earthbreaker.namu.core.domain.member.friend.FriendRelationCommand;
+import univ.earthbreaker.namu.external.notification.FollowPushNotificationSourceCommand;
+import univ.earthbreaker.namu.external.notification.NotificationPort;
 
 @RestController
 @RequestMapping("/v1/friends")
 public class FriendFollowController {
 
 	private final FriendFollowService friendFollowService;
+	private final NotificationPort notificationPort;
 
-	public FriendFollowController(FriendFollowService friendFollowService) {
+	public FriendFollowController(FriendFollowService friendFollowService, NotificationPort notificationPort) {
 		this.friendFollowService = friendFollowService;
+		this.notificationPort = notificationPort;
 	}
 
 	@AuthMapping
 	@PostMapping("/follow/{targetMemberNo}")
 	public ResponseEntity<Void> follow(@LoginMember Long memberNo, @PathVariable Long targetMemberNo) {
-		friendFollowService.follow(new FriendRelationCommand(memberNo, targetMemberNo));
+		FollowResult followResult = friendFollowService.follow(new FriendRelationCommand(memberNo, targetMemberNo));
+		notificationPort.sendAfterFollow(new FollowPushNotificationSourceCommand(
+			followResult.memberNickname(),
+			followResult.targetNickname(),
+			followResult.targetTokenValue()
+		));
 		return ResponseEntity.noContent().build();
 	}
 }
