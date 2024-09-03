50c11e8e9c4ec993c1f5ae07eb6f03bf78a29801
chanwoo <chocw0402@gmail.com>
2024-09-03 20:41:20 +0900

    refactor : 선언적 트랜잭션 -> 프로그래매틱 트랜잭션으로 관리 변경

--- src/main/java/univ/earthbreaker/namu/core/domain/mission/MemberMissionCertifyService.java
+++ src/main/java/univ/earthbreaker/namu/core/domain/mission/MemberMissionCertifyService.java
@@ -1,45 +1,71 @@
 package univ.earthbreaker.namu.core.domain.mission;
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import univ.earthbreaker.namu.event.point.AddRewardPointEvent;
-import univ.earthbreaker.namu.event.image.DeleteUploadedImageEvent;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
 import univ.earthbreaker.namu.event.EventPublisher;
+import univ.earthbreaker.namu.event.image.DeleteUploadedImageEvent;
+import univ.earthbreaker.namu.event.point.AddRewardPointEvent;
 import univ.earthbreaker.namu.event.post.PostCreateEvent;
 
 @Service
 public class MemberMissionCertifyService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(MemberMissionCertifyService.class);
+	private static final AtomicBoolean IS_ROLLBACK = new AtomicBoolean(false);
+
 	private final MemberMissionFinder memberMissionFinder;
 	private final MissionCertifyHandler missionCertifyHandler;
 	private final EventPublisher eventPublisher;
+	private final TransactionTemplate transactionTemplate;
 
 	public MemberMissionCertifyService(
 		MemberMissionFinder memberMissionFinder,
 		MissionCertifyHandler missionCertifyHandler,
-		EventPublisher eventPublisher
+		EventPublisher eventPublisher,
+		PlatformTransactionManager platformTransactionManager
 	) {
 		this.memberMissionFinder = memberMissionFinder;
 		this.missionCertifyHandler = missionCertifyHandler;
 		this.eventPublisher = eventPublisher;
+		this.transactionTemplate = new TransactionTemplate(platformTransactionManager);
 	}
 
-	@Transactional
 	public void successMission(
 		@NotNull MissionCompleteCommand missionCommand,
 		@NotNull CertifiedMissionPostCommand postCommand
 	) {
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(@NotNull TransactionStatus status) {
+				try {
+					MemberMission memberMission = memberMissionFinder.find(
+						missionCommand.getMemberNo(), missionCommand.getMissionNo());
+					MemberMission successMission = missionCertifyHandler.success(memberMission);
+					publishRewardEventForSuccessMission(successMission);
+					publishCreatePostEventForSuccessMission(postCommand, successMission);
+				} catch (Exception e) {
+					LOGGER.info("예외 발생으로 인한 트랜잭션 롤백 및 이미지 삭제 : {}", e.getMessage());
+					status.setRollbackOnly();
+					IS_ROLLBACK.set(status.isRollbackOnly());
+				}
+			}
+		});
 		publishDeleteUploadImageEventWhenTransactionRollback(postCommand.getImagePathKey());
-		MemberMission memberMission = memberMissionFinder.find(missionCommand.getMemberNo(), missionCommand.getMissionNo());
-		MemberMission successMission = missionCertifyHandler.success(memberMission);
-		publishRewardEventForSuccessMission(successMission);
-		publishCreatePostEventForSuccessMission(postCommand, successMission);
 	}
 
 	private void publishDeleteUploadImageEventWhenTransactionRollback(@NotNull String imagePathKey) {
-		eventPublisher.publish(new DeleteUploadedImageEvent(imagePathKey));
+		if (IS_ROLLBACK.get()) {
+			eventPublisher.publish(new DeleteUploadedImageEvent(imagePathKey));
+		}
 	}
 
 	private void publishRewardEventForSuccessMission(@NotNull MemberMission successMission) {
