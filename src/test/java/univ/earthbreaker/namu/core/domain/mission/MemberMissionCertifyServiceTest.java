0ce616cefd3c8f70710636b77727b19b36c3a889
chanwoo <chocw0402@gmail.com>
2024-09-04 15:21:55 +0900

    test : 트랜잭션 관리 변경으로 인한 테스트 코드 추가

--- src/test/java/univ/earthbreaker/namu/core/domain/mission/MemberMissionCertifyServiceTest.java
+++ src/test/java/univ/earthbreaker/namu/core/domain/mission/MemberMissionCertifyServiceTest.java
@@ -1,6 +1,10 @@
 package univ.earthbreaker.namu.core.domain.mission;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static univ.earthbreaker.namu.core.domain.mission.MissionFixture.DEFAULT_MISSION_NO;
@@ -13,6 +17,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import univ.earthbreaker.namu.event.EventPublisher;
 import univ.earthbreaker.namu.event.image.DeleteUploadedImageEvent;
@@ -28,19 +35,27 @@ class MemberMissionCertifyServiceTest {
 	private @Mock MemberMissionFinder memberMissionFinder;
 	private @Mock MissionCertifyHandler missionCertifyHandler;
 	private @Mock EventPublisher eventPublisher;
+	private @Mock TransactionTemplate transactionTemplate;
 	private @InjectMocks MemberMissionCertifyService memberMissionCertifyService;
 
-	@DisplayName("""
-		회원의 인증 성공한 미션을 찾아와 '미션 성공'상태로 변경하고,
-		보상 포인트 지급 이벤트와 게시글 업로드 이벤트, 롤백 발생 시 업로드한 이미지 삭제 이벤트를 발행한다""")
+	@DisplayName("회원의 인증 성공한 미션을 찾아와 '미션 성공'상태로 변경하고, 보상 포인트 지급 이벤트와 게시글 업로드 이벤트를 발행한다")
 	@Test
-	void successMission() {
+	void successMission_and_publishEventInTransaction() {
 		// given
-		when(memberMissionFinder.find(MEMBER_NO, DEFAULT_MISSION_NO))
-			.thenReturn(DEFAULT_MISSION_READY);
-		MemberMission successMission = DEFAULT_MISSION_READY.success();
-		when(missionCertifyHandler.success(DEFAULT_MISSION_READY))
-			.thenReturn(successMission);
+		when(transactionTemplate.execute(any(TransactionCallbackWithoutResult.class)))
+			.thenAnswer(invocation -> {
+				TransactionCallbackWithoutResult callback = invocation.getArgument(0);
+				TransactionStatus status = mock(TransactionStatus.class);
+
+				when(memberMissionFinder.find(MEMBER_NO, DEFAULT_MISSION_NO))
+					.thenReturn(DEFAULT_MISSION_READY);
+				when(missionCertifyHandler.success(DEFAULT_MISSION_READY))
+					.thenReturn(DEFAULT_MISSION_READY.success());
+
+				callback.doInTransaction(status);
+
+				return null;
+			});
 
 		// when
 		memberMissionCertifyService.successMission(
@@ -49,6 +64,7 @@ class MemberMissionCertifyServiceTest {
 		);
 
 		// then
+		MemberMission successMission = DEFAULT_MISSION_READY.success();
 		assertAll(
 			() -> verify(eventPublisher)
 				.publish(new AddRewardPointEvent(successMission.getMemberNo(), successMission.getRewardPoint())),
@@ -56,9 +72,39 @@ class MemberMissionCertifyServiceTest {
 				.publish(new PostCreateEvent(
 					MEMBER_NO, successMission.getActivity(), MISSION_POST_CONTENT,
 					MISSION_POST_IMAGE_PATH_KEY, successMission.getNo()
-				)),
-			() -> verify(eventPublisher).publish(new DeleteUploadedImageEvent(MISSION_POST_IMAGE_PATH_KEY))
+				))
 		);
+	}
+
+	@DisplayName("트랜잭션 내에서 예외가 발생하면 트랜잭션을 롤백하고 이미지 삭제 이벤트를 발행한다")
+	@Test
+	void rollbackTransactionAndTriggerImageDeletionEventOnException() {
+	    // given
+		when(transactionTemplate.execute(any(TransactionCallbackWithoutResult.class)))
+			.thenAnswer(invocation -> {
+				TransactionCallbackWithoutResult callback = invocation.getArgument(0);
+				TransactionStatus status = mock(TransactionStatus.class);
+
+				doThrow(new RuntimeException("테스트 런타임 예외 발생!"))
+					.when(missionCertifyHandler).success(any(MemberMission.class));
+				doAnswer(invocationOnMock -> {
+					when(status.isRollbackOnly()).thenReturn(true);
+					return null;
+				}).when(status).setRollbackOnly();
+
+				callback.doInTransaction(status);
+
+				return null;
+			});
+
+	    // when
+		memberMissionCertifyService.successMission(
+			new MissionCompleteCommand(MEMBER_NO, DEFAULT_MISSION_NO),
+			new CertifiedMissionPostCommand(MEMBER_NO, MISSION_POST_CONTENT, MISSION_POST_IMAGE_PATH_KEY)
+		);
+
+	    // then
+		verify(eventPublisher).publish(new DeleteUploadedImageEvent(MISSION_POST_IMAGE_PATH_KEY));
 	}
 
 	@DisplayName("회원의 인증 성공한 미션을 찾아와 '미션 실패'상태로 변경한다")
