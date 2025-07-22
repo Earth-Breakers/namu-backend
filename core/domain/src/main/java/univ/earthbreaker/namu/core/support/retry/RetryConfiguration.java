9b42ffe1570ca4eec5a8a8ed9f2968a7748f9abb
chanwoo <chocw0402@gmail.com>
2025-07-22 17:27:21 +0900

    refactor : @EnableRetry 추가

--- core/domain/src/main/java/univ/earthbreaker/namu/core/support/retry/RetryConfiguration.java
+++ core/domain/src/main/java/univ/earthbreaker/namu/core/support/retry/RetryConfiguration.java
@@ -2,10 +2,12 @@ package univ.earthbreaker.namu.core.support.retry;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
+@EnableRetry
 @Configuration
 public class RetryConfiguration {
 
